f6e8e08746c9e66fbe152fefd85717add070df72
zangurex1998 <gzanguridze@gmail.com>
2023-02-21 15:45:43 +0400

    Done Summer and Winter Hotels Configuration
     Checked and everything is fine, just need to change design a little bit

--- TripGuide/Model/seaside.swift
+++ TripGuide/Model/seaside.swift
@@ -19,4 +19,16 @@ struct Seasides: Codable{
     let hotelRate: Double
     let hotelInfo: String
     let hotelPrice: String
+    let wifi: Bool // amenities
+    let gym: Bool // amenities
+    let freeParking: Bool // amenitiess
+    let swimmingPool: Bool //amenities
+    let minibar: Bool // amenities
+    let familyRooms: Bool // amenities
+    let freeBreakfast: Bool // amenities
+    let privateBalcony: Bool //roomFeatures
+    let roomService: Bool //roomFeatures
+    let refrigerator: Bool //roomFeatures
+    let nonSmokingRoom: Bool // roomFeatures
+    let romantic: Bool // roomFeatures
 }
--- TripGuide/Scenes/Favourites/Seaside/SeeAllFiles/seeAllView/Summer.swift
+++ TripGuide/Scenes/Favourites/Seaside/SeeAllFiles/seeAllView/Summer.swift
@@ -29,13 +29,14 @@ class SummerViewController: UIViewController {
             self?.summer = summ.seasides
         }
             setUpTableView()
+        navigationItem.backButtonTitle = ""
     }
     
     private func setUpTableView(){
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UINib(nibName: "SummerTableViewCell", bundle: nil), forCellReuseIdentifier: "SummerTableViewCell")
-        
+        tableView.backgroundColor = .black
     }
     
     
@@ -54,6 +55,29 @@ extension SummerViewController: UITableViewDataSource, UITableViewDelegate{
         let cell = tableView.dequeueReusableCell(withIdentifier: "SummerTableViewCell", for: indexPath) as! SummerTableViewCell
         cell.configure(with: summer[indexPath.row])
         return  cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = UIStoryboard(name: "WinterHDetails", bundle: nil).instantiateViewController(withIdentifier: "WinterHDetails") as! WHotelsDetailsViewController
+        vc.hotelString = summer[indexPath.row].hotelImage
+        vc.hotelNameTitle = summer[indexPath.row].hotelName
+        vc.hName = summer[indexPath.row].hotelName
+        vc.rate = summer[indexPath.row].hotelRate
+        vc.wifi = summer[indexPath.row].wifi
+        vc.gymString = summer[indexPath.row].gym
+        vc.parkingString = summer[indexPath.row].freeParking
+        vc.poolString = summer[indexPath.row].swimmingPool
+        vc.minibar = summer[indexPath.row].minibar
+        vc.breakfast = summer[indexPath.row].freeBreakfast
+        vc.hotelDescription = summer[indexPath.row].hotelInfo
+        vc.roomSrvc = summer[indexPath.row].roomService
+        vc.balcony = summer[indexPath.row].privateBalcony
+        vc.noSmoke = summer[indexPath.row].nonSmokingRoom
+        vc.refrig = summer[indexPath.row].refrigerator
+        vc.romantic = summer[indexPath.row].romantic
+        
+        navigationController?.pushViewController(vc, animated: true)
         
     }
 
