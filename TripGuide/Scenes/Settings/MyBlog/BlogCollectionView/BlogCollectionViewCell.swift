413c23e7faa20eedb9a16504d5dbbd8bd1352a59
zangurex1998 <gzanguridze@gmail.com>
2023-02-23 20:09:33 +0400

    Updated but need design changes
    added blog page

--- TripGuide/Scenes/Settings/MyBlog/BlogCollectionView/BlogCollectionViewCell.swift
+++ TripGuide/Scenes/Settings/MyBlog/BlogCollectionView/BlogCollectionViewCell.swift
@@ -6,21 +6,31 @@
 //
 
 import UIKit
-
+import Kingfisher
 class BlogCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var headerLbl: UILabel!
-    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var textLbl: UILabel!
+    
+    @IBOutlet weak var touristImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.backgroundColor = .red
+        
         setUpContentView()
     }
     
     
     private func setUpContentView(){
-        textView.backgroundColor = .black
-        
+        textLbl.backgroundColor = .black
+        textLbl.textColor = .white
+        headerLbl.textColor = .white
+        touristImage.kf.setImage(with: URL(string: "https://png.pngtree.com/png-vector/20220716/ourmid/pngtree-confused-tourist-man-and-woman-search-way-vector-png-image_6001995.png"))
+        touristImage.layer.cornerRadius = touristImage.frame.width / 2
+        self.layer.cornerRadius = 5
+        self.layer.borderColor = UIColor.systemBlue.cgColor
+        self.layer.borderWidth = 0.5
     }
     
     
